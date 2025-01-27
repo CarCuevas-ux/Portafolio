148e1c9f02833ffca3bd12ba3492fb972d721d88
CarCuevas-ux <carloscuevas61@gmail.com>
2025-01-27 16:54:32 -0300

    Primer Commit

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll('.fade-in');
+    const darkModeButton = document.getElementById('modoOscuroBtn');
+
+    // Configuración de IntersectionObserver
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in-visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.5
+    });
+
+    // Observar cada sección con la clase 'fade-in'
+    sections.forEach(section => observer.observe(section));
+
+    // Función para inicializar el modo oscuro
+    const initializeDarkMode = () => {
+        const isDarkModeActive = localStorage.getItem('modoOscuro') === 'true';
+        document.body.classList.toggle('modo-oscuro', isDarkModeActive);
+        updateDarkModeIcon(isDarkModeActive);
+    };
+
+    // Función para actualizar el icono del botón de modo oscuro
+    const updateDarkModeIcon = (isDarkMode) => {
+        const icon = darkModeButton.querySelector('i');
+        if (icon) {
+            icon.classList.toggle('fa-moon', isDarkMode);
+            icon.classList.toggle('fa-sun', !isDarkMode);
+        }
+    };
+
+    // Evento para alternar el modo oscuro
+    darkModeButton.addEventListener('click', () => {
+        const isDarkMode = document.body.classList.toggle('modo-oscuro');
+        updateDarkModeIcon(isDarkMode);
+        localStorage.setItem('modoOscuro', isDarkMode);
+    });
+
+    // Inicializar el estado del modo oscuro al cargar la página
+    initializeDarkMode();
+});
